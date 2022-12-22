00dd2325148d2f4607818fe130cafe5ac7f69981
Heinrich Maël <mael.heinrich@etu.unistra.fr>
2022-12-22 18:45:36 +0100

    RE  not finish mouvement

--- src/projet_exploration/App/Fenetre.java
+++ src/projet_exploration/App/Fenetre.java
@@ -2,22 +2,23 @@ package projet_exploration.App;
 import java.awt.Color;
 import java.awt.event.*;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+//import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Fenetre extends JFrame implements KeyListener{
     public Grille G;
     
-    JLabel label;  
+    JPanel panel;  
  
     public Fenetre(String str){
         super(str);
         
-        label= new JLabel();
-        label.setSize(300,300);
-        label.setBackground(Color.red);
-        label.setOpaque(true);
+        panel= new JPanel();
+        panel.setSize(300,300);
+        panel.setBackground(Color.red);
+        panel.setOpaque(true);
         
-        this.add(label);
+        this.add(panel);
         this.setVisible(true);
         
     }
@@ -36,13 +37,13 @@ public class Fenetre extends JFrame implements KeyListener{
     @Override
     public void keyTyped(KeyEvent arg0) {
         switch(arg0.getKeyChar()) {
-        case 'q': label.setLocation(label.getX()-100,label.getY());
+        case 'q': panel.setLocation(panel.getX()-300,panel.getY());
         	break;
-        case'z': label.setLocation(label.getX(),label.getY()-100);
+        case'z': panel.setLocation(panel.getX(),panel.getY()-300);
     		break;
-        case's':label.setLocation(label.getX(),label.getY()+100);
+        case's':panel.setLocation(panel.getX(),panel.getY()+300);
     		break;
-        case'd': label.setLocation(label.getX()+100,label.getY());
+        case'd': panel.setLocation(panel.getX()+300,panel.getY());
     		break;
         
         }
