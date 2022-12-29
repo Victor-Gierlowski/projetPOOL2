9cb30ece2add66953ed9f791a5962e1dab498272
Victor-Gierlowski <59644630+Victor-Gierlowski@users.noreply.github.com>
2022-12-29 22:18:10 +0100

    Commentaire.. +ajout de commentaire dans Game.java

--- src/projet_exploration/App/Game.java
+++ src/projet_exploration/App/Game.java
@@ -29,27 +29,51 @@ public class Game  implements Runnable{
 		
 		private long lastFrame = 0;
 	public void start(String[] args) {
+		//Définie l'objet affichage qui gère l'interface
 		affichage = new UI();
+		// La fenetre
 		frame = new Fenetre("notre jeu",this);
+		//Définit la taille de notre fenetre et sa position d'apparition
         frame.setSize(1000,800);
         frame.setLocation(200,200);
         //frame.G = Grille.fromTxt("level1.txt");
+        
+        
+        //Ici on lis un fichier json qui contient des salles contigue
         this.loadLevelFromJSON("levels/level1.json");
+        //On définit la grille de travail et d'affichage sur celle 
+        //du premier niveau
         frame.G = levels.get(0);
         affichage.G = frame.G;
-
-        ImageIcon perso = new ImageIcon("./img/personnage1.png");
+        
+        //On récupère la position d'apparition du joueur
         Point s = jsonLevels.get(0).getSpawn();
+        //On créer le joueur et le stock dans game.joueur
         this.joueur = new Joueur(jsonMaster.health,jsonMaster.degats);
+        //On créer la CASE du joueur aux positions de spawn,
+        //et en la reliant au joueur.
         CasePerso joueur = new CasePerso(s.x, s.y, ((Personnage)this.joueur));
+        //On définit le tableau des ennemies
         affichage.Persos = new ArrayList<CasePerso>();
+        //On modifie pour dire que les cases sur lesquelles
+        //spawn les monstres, on ne peut pas marcher
+        //car déjà occupé
+        //(uniquement utile tour 1
         for(CasePerso m : affichage.Persos) {
         	affichage.G.map[m.x][m.y].movable =false;
         }
+        //On fait le lien pour la CASE joueur dans affichage
+        //donc la même que frame | UI & fenetre
         affichage.joueur = joueur;
+        //On définit les monstres sur les monstre du niveau
         affichage.Persos = jsonLevels.get(0).monstres;
+        
+        // Frame n'a pas besoin de la case mais
+        // du joueur car interraction de jeu pas affichage.
         frame.joueur = (Joueur) joueur.getP();
+        //On ajoute le système d'affichage à la fentre
         frame.add(affichage);
+        //On rajoute le lien entre la fenetre et l'affichage
         frame.affichage = affichage;
         
 		int i,j;
